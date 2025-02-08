2116436b40fc1b5c078ca879fdcf4e9eca292d82
Ayoub Jdad <ayoub.jdad@weexa.com>
2025-02-08 15:15:05 +0100

    front: remove loading

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,9 +3,9 @@ import { DataContext } from "./context/DataProvider";
 import Dashboard from "./pages/dashboard/Dashboard";
 
 export default function App() {
-  const { fetched } = useContext(DataContext);
+  // const { fetched } = useContext(DataContext);
 
-  if (!fetched) return <h1>Loading...</h1>;
+  // if (!fetched) return <h1>Loading...</h1>;
 
   return <Dashboard />;
 }
